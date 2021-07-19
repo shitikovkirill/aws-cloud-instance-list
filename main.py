822df979d1d71f1411dd843e02c18d541820c3e8
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-19 08:16:35 +0300

    Add table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+from tabulate import tabulate
 
 from aws import get_instance_detail, get_instance_vpc, get_instances_from_vpc
 
@@ -9,6 +10,17 @@ def info():
     detail = get_instance_detail()
     vpc = get_instance_vpc(detail["instanceId"], detail["region"])
     instances = get_instances_from_vpc(vpc, detail["region"])
+    table = []
+    headers = ["Ip", "Name"]
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            private_ip_addr = instance['PrivateIpAddress']
+            table.append([
+                private_ip_addr,
+                [inst['Value'] for inst in instance['Tags']
+                 if inst['Key'] == 'Name'][0]
+            ])
+    print(tabulate(table, headers, tablefmt="grid"))
 
 
 if __name__ == '__main__':
