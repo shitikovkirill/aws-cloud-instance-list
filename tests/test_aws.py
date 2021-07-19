42389336e9e5234e9413efb6bfeda4d1505a2a5f
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-19 07:15:09 +0300

    Fix test

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,8 +1,12 @@
+import re
+
 from src.aws import get_instance_detail, get_token
 
 
 def test_get_token():
-    assert get_token() == ""
+    regex = re.compile(r'(?:\w+\s+)([a-zA-Z_][a-zA-Z0-9]+)\b')
+    token = get_token()
+    assert re.search(regex, token)
 
 
 def test_instance_detail():
