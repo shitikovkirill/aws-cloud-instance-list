0610155307b190202caac0052bd00d5a8ce5ca0f
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-19 09:04:13 +0300

    Update install_requires

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import setuptools
+from distutils.core import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setuptools.setup(
+setup(
     name="aws-cloud-instance-list",
     version="0.0.1",
     description="Aws cloud instance list",
     author="Kirill Shitikov",
     author_email="sh.kiruh@gmail.com",
     packages=setuptools.find_packages(where="cloud_instance_list"),
-    install_requires=required,
+    install_requires=[
+        "click==8.0.1",
+        "boto3==1.18.1",
+        "requests==2.26.0",
+        "tabulate==0.8.9"
+    ],
     extras_require={
         'dev': [
             'pytest',
