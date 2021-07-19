631fc7c19e18c247b311c2e0866749288734ef0c
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-19 07:11:54 +0300

    Fix get_token

--- src/aws.py
+++ src/aws.py
@@ -7,7 +7,7 @@ def get_token():
         "http://169.254.169.254/latest/api/token",
         headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
     )
-    return token
+    return token.text
 
 
 def get_instance_detail():
