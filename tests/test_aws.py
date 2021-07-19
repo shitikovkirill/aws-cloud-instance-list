48631db2afe08259eb8b011661c1bce75bbf2f4d
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-19 07:07:56 +0300

    Fix tests

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1,5 +1,9 @@
-from src.aws import get_instance_detail
+from src.aws import get_instance_detail, get_token
+
+
+def test_get_token():
+    assert get_token() == ""
 
 
 def test_instance_detail():
-    assert get_instance_detail()
+    assert get_instance_detail() == ""
