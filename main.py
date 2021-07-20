16421b51e853487f903393d1c2ed0094b242018e
EC2 Default User <ec2-user@ip-172-31-22-34.eu-central-1.compute.internal>
2021-07-20 18:18:43 +0000

    Fix

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def info():
     vpc = get_instance_vpc(detail["instanceId"], detail["region"])
     instances = get_instances_from_vpc(vpc, detail["region"])
 
-    headers = ["Private IP", "Instance Name"]
+    headers = ["Private IP", "mssh command", "Instance Name"]
     table = []
     for reservation in instances['Reservations']:
         for instance in reservation['Instances']:
             private_ip_addr = instance['PrivateIpAddress']
             table.append([
                 private_ip_addr,
+                f'mssh {instance["InstanceId"]} -r {detail["region"]}',
                 [inst['Value'] for inst in instance['Tags']
                  if inst['Key'] == 'Name'][0]
             ])
