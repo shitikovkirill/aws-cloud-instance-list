d968267b76f59d7654293f242830b5adfe16d991
Shitikov Kirill <k.shitikov@itransition.com>
2021-07-29 00:03:37 +0300

    Fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="aws-cloud-instance-list",
-    version="0.0.3",
+    version="0.0.4",
     description="Aws cloud instance list",
     author="Kirill Shitikov",
     author_email="sh.kiruh@gmail.com",
